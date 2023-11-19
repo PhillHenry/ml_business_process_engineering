c37175ae2c78e9f23d91fbf45a8e78dd6c85221f
PhillipHenry <PhillHenry@gmail.com>
2023-11-19 13:56:16 +0000

    spectral

--- hugo/build_site.sh
+++ hugo/build_site.sh
@@ -1,5 +1,5 @@
 rm -rf public/* && hugo -verbose && ls public/ && \
 for FILE in $(find public/ -name \*.xml) ; do { echo Deleting $FILE ; rm $FILE ; } done && \
-rm -rf ../docs && mv public ../docs  && \
+for FILE in $(find ../docs/ | grep -v CNAME) ; do { echo Deleting $FILE ; rm $FILE ; } done && cp -r public/* ../docs/ && \
 for FILE in $(find ../docs/) ; do { echo Adding $FILE ; git add $FILE ; } done && git commit ../docs -m "rebuilt site" && \
 echo Now run git push
