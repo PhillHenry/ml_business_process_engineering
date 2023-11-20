781f948960d43e6fb208942ef0afb5ca8e00716a
PhillipHenry <PhillHenry@gmail.com>
2023-11-20 09:44:24 +0000

    better deleting

--- hugo/build_site.sh
+++ hugo/build_site.sh
@@ -1,4 +1,4 @@
-rm -rf public/* && hugo -verbose && ls public/ && \
+rm -rf public/* && hugo -D -verbose && ls public/ && \
 for FILE in $(find public/ -name \*.xml) ; do { echo Deleting $FILE ; rm $FILE ; } done && \
 for FILE in $(find ../docs/ | grep -v CNAME) ; do { echo Deleting $FILE ; rm $FILE ; } done && cp -r public/* ../docs/ && \
 for FILE in $(find ../docs/) ; do { echo Adding $FILE ; git add $FILE ; } done && git commit ../docs -m "rebuilt site" && \
